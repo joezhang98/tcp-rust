d81d8a6c7364fdeb1e93ad605d42d50d2890aa0b
Joe Zhang <zz98@stanford.edu>
2021-03-21 13:46:54 -0700

    prog: now can do SYN/ACK, working on establishing connection

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,16 +1,16 @@
 use std::io;
 
 enum State {
-    Closed,
-    Listen,
+    // Listen,
     SynRcvd,
-    // Estab,
+    Estab,
 }
 
 pub struct Connection {
     state: State,
     send: SendSequenceSpace,
     recv: RecvSequenceSpace,
+    ip: etherparse::Ipv4Header,
 }
 
 /// State of the Send Sequence Space (RFC 793 S3.2 F4)
@@ -98,7 +98,24 @@ impl Connection {
                 nxt: tcph.sequence_number() + 1,
                 wnd: tcph.window_size(),
                 up: false,
-            }
+            },
+            ip: etherparse::Ipv4Header::new(
+                0,
+                64, 
+                etherparse::IpTrafficClass::Tcp,
+                [
+                    iph.destination()[0],
+                    iph.destination()[1],
+                    iph.destination()[2],
+                    iph.destination()[3],
+                ],
+                [
+                    iph.source()[0],
+                    iph.source()[1],
+                    iph.source()[2],
+                    iph.source()[3],
+                ],
+            ),
         };
 
         // need to start establishing a connection
@@ -112,28 +129,16 @@ impl Connection {
         syn_ack.acknowledgment_number = c.recv.nxt;
         syn_ack.syn = true;
         syn_ack.ack = true;
-        let mut ip = etherparse::Ipv4Header::new(
-            syn_ack.header_len(),
-            64, 
-            etherparse::IpTrafficClass::Tcp,
-            [
-                iph.destination()[0],
-                iph.destination()[1],
-                iph.destination()[2],
-                iph.destination()[3],
-            ],
-            [
-                iph.source()[0],
-                iph.source()[1],
-                iph.source()[2],
-                iph.source()[3],
-            ],
-        );
+        c.ip.set_payload_len(syn_ack.header_len() as usize + 0);
+        // the kernel is nice and does this for us
+        // syn_ack.checksum = syn_ack
+        //     .calc_checksum_ipv4(&ip, &[])
+        //     .expect("failed to compute checksum");
 
         // write out the headers
         let unwritten = {
             let mut unwritten = &mut buf[..];
-            ip.write(&mut unwritten);
+            c.ip.write(&mut unwritten);
             syn_ack.write(&mut unwritten);
             unwritten.len()
         };
@@ -148,6 +153,32 @@ impl Connection {
         tcph: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
     ) -> io::Result<()> {
-        unimplemented!();
+        // acceptable ack check
+        // SND.UNA < SEG.ACK =< SND.NXT
+        // but remember wrapping!
+        let ackn = tcph.acknowledgment_number();
+        if self.send.una < ackn {
+            // check is violated iff n is between u and a
+            if self.send.nxt >= self.send.una && self.send.nxt < ackn {
+                return Ok(());
+            }
+        } else {
+            // check is okay iff n is between u and a
+            if self.send.nxt >= ackn && self.send.nxt < self.send.una {
+            } else {
+                return Ok(());
+            }
+        }
+
+        match self.state {
+            State::SynRcvd => {
+                // expect to get an ACK for our SYN
+            }
+            State::Estab => {
+                unimplemented!();
+            }
+        }
+
+        Ok(())
     }
 }
